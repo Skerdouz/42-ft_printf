1e2992f6ee148fb028b17f9c250a5bf364113230
Skerdouz <lucasbrahinski60@gmail.com>
2024-05-30 19:04:46 +0200

    fixed %p return values

--- src/argprint/print_ptr.c
+++ src/argprint/print_ptr.c
@@ -5,8 +5,10 @@ int	print_ptr(void *ptr)
 	int	bytesread;
 
 	bytesread = 0;
-	if (!ptr)
-		return (print_str("(nil)"));
+	if (ptr == (int *)0x0)
+		return (write(1, "0x0", 3));
+	if (ptr == NULL)
+		return (write(1, "(nil)", 5));
 	bytesread = write(1, "0x", 2);
 	bytesread += print_hex((size_t)ptr, 'x');
 	return (bytesread);
